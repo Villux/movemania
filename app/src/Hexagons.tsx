f369b69689b34c20b20b36014ac709ee5d213179
Teemu Taskula <themetor@gmail.com>
2023-11-11 20:48:07 +0200

    Update hexagon coloring

--- app/src/Hexagons.tsx
+++ app/src/Hexagons.tsx
@@ -10,16 +10,26 @@ export function Hexagons({ hexagons }: Props) {
   return (
     <>
       {hexagons.map(({ h3Index, capturedBy }) => {
-        const capturedCount = capturedBy?.length ?? 0;
+        let fillColor = "rgba(255, 245, 0, 0.04)";
+        let strokeColor = "#000";
 
-        // Increase opacity of hexagon fill color based on number of players
-        // that have captured it.
-        const fillColor = `rgba(255, 245, 0, ${Math.min(
-          1,
-          0.08 + capturedCount * 0.25
-        )})`;
+        if (capturedBy.includes("Teemu")) {
+          
 
-        const strokeColor = capturedCount > 0 ? "#fff385" : "#000";
+          // Me and others have captured this hexagon
+          if (capturedBy.length > 1) {
+            fillColor = "rgba(255, 245, 0, 0.75)";
+            strokeColor = "rgba(255, 245, 0, 1)";
+          } else {
+            // Only I have captured this hexagon
+            fillColor = "rgba(255, 245, 0, 0.4)";
+            strokeColor = "rgba(255, 245, 0, 0.5)";
+          }
+        } else if (capturedBy.length > 0) {
+          // Someone else has captured this hexagon
+          fillColor = "rgba(255, 245, 0, 0.15)";
+          strokeColor = "rgba(255, 245, 0, 0.2)";
+        }
 
         const coordinates = h3.cellToBoundary(h3Index).map(([lat, lng]) => ({
           latitude: lat,
