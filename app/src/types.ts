7467864be57560e3839a482bf12e04602052e59b
Teemu Taskula <themetor@gmail.com>
2023-11-11 19:11:50 +0200

    Add fake next-level phase

--- app/src/types.ts
+++ app/src/types.ts
@@ -12,7 +12,12 @@ export type Hexagon = {
   coordinate: Coordinate;
 };
 
-export type GamePhase = "start" | "play" | "stats" | "highlights";
+export type GamePhase =
+  | "start"
+  | "play"
+  | "stats"
+  | "highlights"
+  | "next-level"; // fake phase for displaying the next level
 
 export type GameStats = Record<Reward, number>;
 
